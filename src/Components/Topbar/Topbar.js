14aefc01cea2f092e1624bc7a70ddf4363811741
Simone Fiore <simone.fiore@aulab.it>
2021-11-02 13:55:33 +0100

    blu

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -15,7 +15,7 @@ export default function Topbar() {
 
       <Link to="/" className={classes.home}>
         {/* <span className="text-yellow-300">SimoneFiore</span> */}
-        <i className="fal fa-hexagon text-yellow-300"></i>
+        <i className="fal fa-hexagon text-white "></i>
       </Link>
       <div className="flex flex-col h-32 w-20 justify-between self-start pt-4 hidden ">
         <Link to="/home1" className={classes.btn}>

2b2fb2475e0c67f23c9c12a1937bf0f51c37e8a1
Simone Fiore <simone.fiore@aulab.it>
2021-11-03 12:39:16 +0100

    d

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       {/* <Navbar /> */}
-      {/* <Topbar /> */}
+      <Topbar />
       <Switch>
         <Route exact path="/">
           {/* <Moon /> */}
